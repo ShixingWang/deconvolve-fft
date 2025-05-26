1e95bdac824360f96ba6293e84f50281f258c63b
ShixingWang <wangshixing@wustl.edu>
2025-05-26 17:14:35 -0500

    Update find_beads.py

--- test/find_beads.py
+++ test/find_beads.py
@@ -1,7 +1,25 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import io,util,filters,feature
+from pathlib import Path
+from skimage import io,util,filters,feature,draw
+
+# %%
+def find_beads(image,z):
+
+    return beads,coordinates
+
+for filepath in Path("data/clean").glob("*FOV-1*.tiff"):
+    img = io.imread(str(filepath))
+    beads,coordinates = find_beads(img,z=12)
+
+
+for filepath in Path("data/clean").glob("*FOV-1*.tiff"):
+    img = io.imread(str(filepath))
+    beads,coordinates = find_beads(img,z=20)
+    
+
+
 
 # %% test on 1 image
 filepath = "data/clean/clean_FOV-1_DAPI.tiff"
@@ -9,7 +27,31 @@ image = io.imread(str(filepath))
 
 # 
 # %%
-coordinates = feature.peak_local_max(image,min_distance=20)
+smoothed = filters.gaussian(image,sigma=1,preserve_range=True)
+io.imsave(
+    "data/smooth_FOV-1_DAPI.tiff",
+    util.img_as_float32(smoothed)
+)
+
 # %%
-image[tuple((c) for c in coordinates)]
+coordinates = feature.peak_local_max(smoothed,min_distance=20,threshold_abs=10)
+markers = np.zeros_like(smoothed,dtype=bool)
+for c in coordinates:
+    rr,cc = draw.disk(c[1:],5)
+    markers[c[0],rr,cc] = True
+io.imsave(
+    "data/markers-3d_FOV-1_DAPI.tiff",
+    util.img_as_ubyte(markers)
+)
+
+# %%
+coordinates = feature.peak_local_max(smoothed[12],min_distance=20,threshold_abs=10)
+markers = np.zeros_like(smoothed[12],dtype=bool)
+for c in coordinates:
+    rr,cc = draw.disk(c,5)
+    markers[rr,cc] = True
+io.imsave(
+    "data/markers-2d_FOV-1_DAPI.tiff",
+    util.img_as_ubyte(markers)
+)
 # %%
