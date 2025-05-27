de9eef954de1f23d1853a5565c30a50596ace735
ShixingWang <wangshixing@wustl.edu>
2025-05-27 17:49:40 -0500

    Create crop_psf.py

---
+++ test/crop_psf.py
@@ -0,0 +1,33 @@
+# %% [markdown] 
+# We have the coordinates of the center of the beads in 2D (from `find_beads.py`)
+# Now we need to crop the point spread function (PSF) around each coordinate.
+# The challenge is that 
+# 1. We set a constant z depth to find maxima, which may not be the true maxima.
+# 2. We do not know the depth, height, and width of each psf.
+# 3. Especially when they could be close to each other and appear in other PSFs
+# Plan:
+# 1. Use voronoi plot to partition the image
+# 2. Find coords of maxima in each masked image.
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from skimage import io,util
+
+# %% Look at the intensity along z direction
+filepath = Path("data/coordinates/FOV-1_DAPI.txt")
+coordinates = np.loadtxt(str(filepath))
+
+image = io.imread(f"data/clean/clean_{filepath.stem}.tiff")
+for coord in coordinates:
+    data = image[:,*[int(c) for c in coord]]
+    plt.figure()
+    plt.plot(data,label=f"{coord}, max at {int(np.argmax(data))}")
+    plt.legend()
+    plt.show()
+    plt.close()
+    # print(f"{coord},\t max at {np.argmax(data)}")
+
+# %%
+voronoi = 
