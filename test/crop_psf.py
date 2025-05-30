1a17edf976e79b350c419a78d9d591459fa08ce1
ShixingWang <wangshixing@wustl.edu>
2025-05-30 17:00:08 -0500

    try find the window of the psf

--- test/crop_psf.py
+++ test/crop_psf.py
@@ -11,10 +11,11 @@
 
 # %%
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 from scipy import spatial,ndimage
-from skimage import io,util
+from skimage import io,util,measure,draw
 
 # %% Look at the intensity along z direction
 filepath = Path("data/coordinates/FOV-1_DAPI.txt")
@@ -34,4 +35,48 @@ for coord in coordinates:
 # %%
 voronoi = spatial.Voronoi(coordinates)
 # %%
-print()
+markers = np.zeros((2044,2048),dtype=int)
+for c,coord in enumerate(coordinates):
+    markers[tuple(coord)] = c+1
+
+distances, (y_idx, x_idx) = ndimage.distance_transform_edt(markers == 0, return_indices=True)
+label_image = markers[y_idx, x_idx]
+# %%
+io.imsave(
+    "data/voronoi_FOV-1_DAPI.tiff",
+    util.img_as_uint(label_image)
+)
+
+# %%
+regions = np.empty_like(image,dtype=int)
+for z in range(regions.shape[0]):
+    regions[z] = label_image
+centroids = measure.regionprops_table(
+    label_image=regions,
+    intensity_image=image,
+    properties=('label',"centroid_weighted")
+)
+plt.hist(centroids["centroid_weighted-0"],bins=20,range=[0,50])
+# This shows that direct weighted mean of the PSF does not give good estimate of center.
+
+# %%
+weighted = np.empty_like(image,dtype=int)
+for e,entry in pd.DataFrame(centroids).iterrows():
+    zz = entry["centroid_weighted-0"]
+    if not 0 <= zz < regions.shape[0]:
+        weighted[:,regions[regions==entry["label"]]] = entry["label"]
+        continue
+    rr,cc = draw.disk((entry["centroid_weighted-1"],entry["centroid_weighted-2"]),5,shape=(2044,2048))
+    weighted[int(zz),rr,cc] = entry['label']
+io.imsave(
+    "data/weighted_FOV-1_DAPI.tiff",
+    util.img_as_uint(weighted)
+)
+    
+
+# %% [markdown] choice of psf window
+# - DAPI:  20*20
+# - FITC:  70 by 70
+# - YFP:   20 by 20 
+# - TRITC: 70 by 70
+#  It's more of a signal-to-noise issue
