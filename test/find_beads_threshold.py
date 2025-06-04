de90f4ca3fb192205e1a480673449070f2acf39d
ShixingWang <wangshixing@wustl.edu>
2025-06-04 14:52:16 -0500

    Update find_beads_threshold.py

--- test/find_beads_threshold.py
+++ test/find_beads_threshold.py
@@ -4,10 +4,26 @@
 
 # %%
 import numpy as np
+import pandas as pd
 from pathlib import Path
 from skimage import io,util,filters,measure,morphology
 
 # %%
+def threshold_stdv(image):
+    threshold = np.mean(image) + 1 * np.std(image)
+    binary = (image > threshold)
+    binary = morphology.binary_opening(binary, morphology.ball(1))
+    return binary
+
+for filepath in Path("data/clean").glob("*.tiff"):
+    img = io.imread(str(filepath))
+    binary = threshold_stdv(img)
+    io.imsave(
+        f"data/segmented/{filepath.stem}.tiff",
+        util.img_as_ubyte(binary)
+    )
+
+# %% test on 1 image
 image = io.imread("data/clean/FOV-1_DAPI.tiff")
 
 # %% result too low
@@ -37,10 +53,41 @@ io.imsave(
 # %%
 threshold = np.mean(image) + 1 * np.std(image)
 binary = (image > threshold)
-binary = morphology.binary_opening(binary, np.ones((3,3,3)))
+binary = morphology.binary_opening(
+    binary, 
+    # np.ones((3,3,3))，
+    morphology.ball(1)
+)
+# %% save
 io.imsave(
     "data/segmented/stdv_FOV-1_DAPI.tiff",
     util.img_as_ubyte(binary)
 )
 
+# there could be false positives in the above thresholded image
+# when the element of binary_opening is a ball(1), 
+# those false positives are also ball(1)
+
 # %%
+filepath = Path("data/segmented/FOV-1_DAPI.tiff")
+mask = io.imread(str(filepath))
+intensities = io.imread(f"data/clean/{filepath.stem}.tiff")
+label_image = measure.label(mask)
+props_table = pd.DataFrame(measure.regionprops_table(
+    label_image=label_image,
+    intensity_image=intensities,
+    properties=('label','area','centroid_weighted','bbox')
+))
+
+# %%
+selected_table = props_table[
+    props_table["area"].gt(20)
+  & props_table["area"].lt(5000)
+]
+
+# %%
+for filepath in Path("data/segmented/").glob("*.tiff"):
+    if "CFP" in filepath.stem:
+        continue
+    break
+
