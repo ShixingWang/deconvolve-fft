5f6c5e47befe978991918d3410bb3403d322c631
ShixingWang <wangshixing@wustl.edu>
2025-06-04 22:20:55 -0500

    update find_beads_threshold.py

--- test/find_beads_threshold.py
+++ test/find_beads_threshold.py
@@ -68,26 +68,65 @@ io.imsave(
 # when the element of binary_opening is a ball(1), 
 # those false positives are also ball(1)
 
-# %%
-filepath = Path("data/segmented/FOV-1_DAPI.tiff")
-mask = io.imread(str(filepath))
-intensities = io.imread(f"data/clean/{filepath.stem}.tiff")
-label_image = measure.label(mask)
-props_table = pd.DataFrame(measure.regionprops_table(
-    label_image=label_image,
-    intensity_image=intensities,
-    properties=('label','area','centroid_weighted','bbox')
-))
+
+# %% [markdown] This script helps find the size ranges of PSFs in each channel. 
+# ```python
+# filepath = Path("data/segmented/FOV-2_FITC.tiff")
+# mask = io.imread(str(filepath))
+# intensities = io.imread(f"data/clean/{filepath.stem}.tiff")
+# label_image = measure.label(mask)
+# props_table = pd.DataFrame(measure.regionprops_table(
+#     label_image=label_image,
+#     intensity_image=intensities,
+#     properties=('label','area','centroid_weighted','bbox')
+# ))
+# selected_table = props_table[
+#     props_table["area"].gt(20)
+#   & props_table["area"].lt(5000)
+# ]
+# ```
 
 # %%
-selected_table = props_table[
-    props_table["area"].gt(20)
-  & props_table["area"].lt(5000)
-]
+ranges = {
+    "DAPI":  (  20, 1000),
+    "FITC":  (1000, 5000),
+    "YFP":   (  20,  200),
+    "TRITC": (1000, 8000),
+}
 
-# %%
-for filepath in Path("data/segmented/").glob("*.tiff"):
-    if "CFP" in filepath.stem:
+channel = "DAPI" # to be turned into a loop 
+labels = io.imread(f"data/segmented/FOV-1_{channel}.tiff")
+intensities = io.imread(f"data/clean/FOV-1_{channel}.tiff")
+
+idx = []
+bbox_z = []
+bbox_r = []
+bbox_c = []
+for prop in measure.regionprops(
+    label_image=labels,
+    intensity_image=intensities
+):
+    if prop.area < ranges[channel][0] or prop.area > ranges[channel][1]:
         continue
-    break
+    
+    img = prop.image_intensity
+    minimum = img[img>0].min()
+    img[img>0] = img[img>0] - minimum
+    img = img / img.sum()
 
+    remeasure = measure.regionprops(
+        label_image=prop.image,
+        intensity_image=img
+    )
+    assert len(remeasure)==1, "Regionprops should return exactly one region"
+    remeasure = remeasure[0]
+    centroids = [int(np.round(c)) for c in remeasure.centroid_weighted]
+    # pad the image to make sure the PSF is centered
+    # wait, should the PSF be centered at centroid or maximum? 
+    io.imsave(
+        f"data/psf/{filepath.stem}_psf-{prop.label}.tiff",
+        util.img_as_float32(img)
+    )
+
+
+# %%
