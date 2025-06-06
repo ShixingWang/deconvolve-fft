05fc9ee2887e01aa49834bbc5078269043708692
ShixingWang <wangshixing@wustl.edu>
2025-06-06 16:55:42 -0500

    get averaged and individual psfs; maybe too many YFP beads

--- test/find_beads_threshold.py
+++ test/find_beads_threshold.py
@@ -5,6 +5,7 @@
 # %%
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from pathlib import Path
 from skimage import io,util,filters,measure,morphology
 
@@ -69,125 +70,151 @@ io.imsave(
 # those false positives are also ball(1)
 
 
-# %% [markdown] This script helps find the size ranges of PSFs in each channel. 
+# %%  This script helps find the size ranges of PSFs in each channel. 
 # ```python
-# filepath = Path("data/segmented/FOV-2_FITC.tiff")
-# mask = io.imread(str(filepath))
-# intensities = io.imread(f"data/clean/{filepath.stem}.tiff")
-# label_image = measure.label(mask)
-# props_table = pd.DataFrame(measure.regionprops_table(
-#     label_image=label_image,
-#     intensity_image=intensities,
-#     properties=('label','area','centroid_weighted','bbox')
-# ))
-# selected_table = props_table[
-#     props_table["area"].gt(20)
-#   & props_table["area"].lt(5000)
-# ]
+filepath = Path("data/segmented/FOV-2_FITC.tiff")
+mask = io.imread(str(filepath))
+intensities = io.imread(f"data/clean/{filepath.stem}.tiff")
+label_image = measure.label(mask)
+props_table = pd.DataFrame(measure.regionprops_table(
+    label_image=label_image,
+    intensity_image=intensities,
+    properties=('label','area','centroid_weighted','bbox')
+))
+selected_table = props_table[
+    props_table["area"].gt(20)
+  & props_table["area"].lt(5000)
+]
+
 # ```
 
 # %%
-ranges = {
-    "DAPI":  (  20, 1000),
-    "FITC":  (1000, 5000),
-    "YFP":   (  20,  200),
-    "TRITC": (1000, 8000),
-}
+def extract_psf(fov,channel):
+    # internal parameters
+    ranges = {
+        "DAPI":  (  20, 1000),
+        "FITC":  (1000, 5000),
+        "YFP":   (  20,  200),
+        "TRITC": (1000, 8000),
+    }
+    # load inputs
+    binary = io.imread(f"data/segmented/FOV-{fov}_{channel}.tiff")
+    labels = measure.label(binary)
+    intensities = io.imread(f"data/clean/FOV-{fov}_{channel}.tiff")
 
-channel = "DAPI" # to be turned into a loop 
-binary = io.imread(f"data/segmented/FOV-1_{channel}.tiff")
-labels = measure.label(binary)
-intensities = io.imread(f"data/clean/FOV-1_{channel}.tiff")
+    # calculate the amount to pad to center the PSFs
+    indices = []
+    centered_z0 = []
+    centered_z1 = []
+    centered_r0 = []
+    centered_r1 = []
+    centered_c0 = []
+    centered_c1 = []
+    centered_tot_z = []
+    centered_tot_r = []
+    centered_tot_c = []
+    cropped = {}
+    for prop in measure.regionprops(
+        label_image=labels,
+        intensity_image=intensities
+    ):
+        if prop.area < ranges[channel][0] or prop.area > ranges[channel][1]:
+            continue
+        
+        img = prop.image_intensity
+        minimum = img[img>0].min()
+        img[img>0] = img[img>0] - minimum
+        img = img / img.sum()
+        cropped[prop.label] = img
 
-indices = []
-bboxes_z = []
-bboxes_r = []
-bboxes_c = []
-centereds = {}
-for prop in measure.regionprops(
-    label_image=labels,
-    intensity_image=intensities
-):
-    if prop.area < ranges[channel][0] or prop.area > ranges[channel][1]:
-        continue
-    
-    img = prop.image_intensity
-    minimum = img[img>0].min()
-    img[img>0] = img[img>0] - minimum
-    img = img / img.sum()
-
-    remeasure = measure.regionprops(
-        label_image=util.img_as_ubyte(prop.image),
-        intensity_image=img
-    )
-    assert len(remeasure)==1, "Regionprops should return exactly one region"
-    remeasure = remeasure[0]
-    
-    required = tuple(int(np.round(2 * c)) for c in remeasure.centroid_weighted)
-    pad_center_left  = np.array(img.shape) - np.array(required)
-    pad_center_left[pad_center_left<0] = 0
-    pad_center_right = np.array(required) - np.array(img.shape)
-    pad_center_right[pad_center_right<0] = 0
-    centered = np.pad(
-        img,
-        (
-            (pad_center_left[0],pad_center_right[0]),
-            (pad_center_left[1],pad_center_right[1]),
-            (pad_center_left[2],pad_center_right[2]),
+        remeasure = measure.regionprops(
+            label_image=util.img_as_ubyte(prop.image),
+            intensity_image=img
         )
-    )
-    bbox_z,bbox_r,bbox_c = centered.shape
-    bboxes_z.append(bbox_z)
-    bboxes_r.append(bbox_r)
-    bboxes_c.append(bbox_c)
-    indices.append(prop.label)
-    centereds[prop.label] = centered
-    # io.imsave(
-    #     f"data/psf/{filepath.stem}_psf-{prop.label}.tiff",
-    #     util.img_as_float32(img)
-    # )
+        assert len(remeasure)==1, "Regionprops should return exactly one region"
+        remeasure = remeasure[0]
+        required = tuple(int(np.round(2 * c)) for c in remeasure.centroid_weighted)
 
-# %%
-bbox_data = pd.DataFrame({
-    "label": indices,
-    "bbox_z": bboxes_z,
-    "bbox_r": bboxes_r,
-    "bbox_c": bboxes_c,
-})
-max_z = bbox_data['bbox_z'].max()
-max_r = bbox_data['bbox_r'].max()
-max_c = bbox_data['bbox_c'].max()
-psf_average = np.zeros((max_z,max_r,max_c))
-count_psf = 0
-for idx in indices:
-    # pad all psfs to have the same size
-    dims = centereds[idx].shape
-    pad_size_z0 = (max_z - dims[0])//2
-    pad_size_z1 = (max_z - dims[0]) - pad_size_z0
-    pad_size_r0 = (max_z - dims[1])//2
-    pad_size_r1 = (max_z - dims[1]) - pad_size_r0
-    pad_size_c0 = (max_z - dims[2])//2
-    pad_size_c1 = (max_z - dims[2]) - pad_size_c0
-    
-    psf = np.pad(
-        centereds[idx],
-        (
-            (pad_size_z0,pad_size_z1),
-            (pad_size_r0,pad_size_r1),
-            (pad_size_c0,pad_size_c1),
+        pad_center_left  = np.array(img.shape) - np.array(required)
+        pad_center_left[pad_center_left<0] = 0
+        pad_center_z0,pad_center_r0,pad_center_c0 = pad_center_left
+        pad_center_right = np.array(required) - np.array(img.shape)
+        pad_center_right[pad_center_right<0] = 0
+        pad_center_z1,pad_center_r1,pad_center_c1 = pad_center_right
+        centered_z0.append(pad_center_z0)
+        centered_z1.append(pad_center_z1)
+        centered_r0.append(pad_center_r0)
+        centered_r1.append(pad_center_r1)
+        centered_c0.append(pad_center_c0)
+        centered_c1.append(pad_center_c1)
+
+        bbox_z,bbox_r,bbox_c = img.shape
+        centered_tot_z.append(pad_center_z0 + bbox_z + pad_center_z1)
+        centered_tot_r.append(pad_center_r0 + bbox_r + pad_center_r1)
+        centered_tot_c.append(pad_center_c0 + bbox_c + pad_center_c1)
+        indices.append(prop.label)
+        # centereds[prop.label] = centered
+
+    bbox_data = pd.DataFrame({
+        "label": indices,
+        "centered_z0": centered_z0,
+        "centered_z1": centered_z1,
+        "centered_r0": centered_r0,
+        "centered_r1": centered_r1,
+        "centered_c0": centered_c0,
+        "centered_c1": centered_c1,
+        "centered_tot_z": centered_tot_z,
+        "centered_tot_r": centered_tot_r,
+        "centered_tot_c": centered_tot_c,
+    })
+    max_z = bbox_data['centered_tot_z'].max()
+    max_r = bbox_data['centered_tot_r'].max()
+    max_c = bbox_data['centered_tot_c'].max()
+    bbox_data.set_index("label",inplace=True)
+
+    # padding the PSFs to the same size
+    psfs = {}
+    for idx in indices:
+        # pad all psfs to have the same size
+        dims_z,dims_r,dims_c = bbox_data.loc[
+                                            idx,
+                                            ['centered_tot_z','centered_tot_r','centered_tot_c']
+                                        ]
+        pad_size_z0 = (max_z - dims_z)//2
+        pad_size_z1 = (max_z - dims_z) - pad_size_z0
+        pad_size_r0 = (max_r - dims_r)//2
+        pad_size_r1 = (max_r - dims_r) - pad_size_r0
+        pad_size_c0 = (max_c - dims_c)//2
+        pad_size_c1 = (max_c - dims_c) - pad_size_c0
+
+        psfs[idx] = np.pad(
+            cropped[idx],
+            (
+                (bbox_data.loc[idx,"centered_z0"]+pad_size_z0, bbox_data.loc[idx,"centered_z1"]+pad_size_z1),
+                (bbox_data.loc[idx,"centered_r0"]+pad_size_r0, bbox_data.loc[idx,"centered_r1"]+pad_size_r1),
+                (bbox_data.loc[idx,"centered_c0"]+pad_size_c0, bbox_data.loc[idx,"centered_c1"]+pad_size_c1),
+            )
         )
-    )
-    io.imsave(
-        f"data/psf/{filepath.stem}_psf-{idx}.tiff",
-        util.img_as_float32(psf)
-    )
+    return psfs
 
-    psf_average += psf
-    count_psf += 1
-psf_average = psf_average/count_psf
-io.imsave(
-    f"data/psf/average_{filepath.stem}.tiff",
-    util.img_as_float32(psf_average)
-)
+# %% 
+for channel in ("DAPI","FITC","YFP","TRITC"):
+    for v in (1,2):
+        psfs = extract_psf(v,channel)
+
+        count_psf = 0
+        psf_average = np.zeros_like(psfs[list(psfs.keys())[0]])
+        for idx in psfs.keys():
+            io.imsave(
+                f"data/psf/psf_FOV-{v}_{channel}_idx-{idx}.tiff",
+                util.img_as_float32(psfs[idx])
+            )
+            psf_average += psfs[idx]
+            count_psf += 1
+        psf_average = psf_average/count_psf
+        io.imsave(
+            f"data/psf/psf-average_FOV-{v}_{channel}.tiff",
+            util.img_as_float32(psf_average)
+        )
 
 # %%
