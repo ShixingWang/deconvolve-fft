270523b60788a40c083d342d0b46c67d8d9900a0
ShixingWang <wangshixing@wustl.edu>
2025-07-01 20:17:39 -0500

    implement cli interfaces

--- deconvolve_fft/cli.py
+++ deconvolve_fft/cli.py
@@ -1,20 +1,34 @@
+import nd2
 import argparse
+import numpy as np
+from pathlib import Path
+from skimage import io,util
+from deconvolve_fft import deconvolve  # Assuming you have a deconvolve_fft function defined in this module
 
 def deconv1img():
-    parser = argparse.ArgumentParser(description="Deconvolve a single image using Fourier Transform.")
+    parser = argparse.ArgumentParser(description="Deconvolve using Fourier Transform a single image.")
     parser.add_argument("--psf",     type=str,   required=True, help="Path to the Point Spread Function (PSF) image.")
     parser.add_argument("--input",   type=str,   required=True, help="Path to the input image file.")
     parser.add_argument("--output",  type=str,   required=True, help="Path to save the deconvolved image.")
     parser.add_argument("--epsilon", type=float, default=1E-6,  help="Tikhonov regularization parameter to avoid division by zero.")
     args = parser.parse_args()
 
-    print("this is deconv1img.")
-    # Here you would call the deconvolution function with the provided arguments
-    # For example:
-    # deconvolve_fft(args.input_image, args.output_image, args.psf, args.iterations)
+    io_psf = nd2 if args.psf.endswith('.nd2') else io
+    img_psf = io_psf.imread(args.psf)
+
+    io_input = nd2 if args.input.endswith('.nd2') else io
+    img_input = io_input.imread(args.input)
+
+    deconvolved = deconvolve(img_input, img_psf, epsilon=args.epsilon)
+    format_function = util.img_as_uint if np.issubdtype(img_input.dtype, np.integer) else util.img_as_float32
+    io.imsave(
+        args.output, 
+        format_function(deconvolved)
+    )
+    return None
 
 def deconv1folder():
-    parser = argparse.ArgumentParser(description="Deconvolve all images in a folder using Fourier Transform.")
+    parser = argparse.ArgumentParser(description="Deconvolve using Fourier Transform all images matching a glob pattern in a folder.")
     parser.add_argument("--psf",     type=str,   required=True, help="Path to the Point Spread Function (PSF) image.")
     parser.add_argument("--input",   type=str,   required=True, help="Path to the input folder containing images, if --glob is not set, all files in it will be iterated over.")
     parser.add_argument("--glob",    type=str,   default="*.tiff",  help="Glob pattern to match the files in the folder to deconvolve. Default is '*.tiff'. ")
@@ -23,8 +37,15 @@ def deconv1folder():
     parser.add_argument("--epsilon", type=float, default=1E-6,      help="Tikhonov regularization parameter to avoid division by zero.")
     args = parser.parse_args()
 
-    print("this is deconv1folder.")
-    # Here you would call the deconvolution function for each image in the folder
-    # For example:
-    # for image_file in os.listdir(args.input_folder):
-    #     deconvolve_fft(os.path.join(args.input_folder, image_file), os.path.join(args.output_folder, image_file), args.psf, args.iterations)
+    io_psf = nd2 if args.psf.endswith('.nd2') else io
+    img_psf = io_psf.imread(args.psf)
+
+    for path_image in Path(args.input).glob(args.glob):
+        io_img = nd2 if path_image.suffix == '.nd2' else io
+        image = io_img.imread(args.input)
+        
+        deconvolved = deconvolve(image, img_psf, epsilon=args.epsilon)
+        format_function = util.img_as_uint if np.issubdtype(image.dtype, np.integer) else util.img_as_float32
+        path_output = f"{args.output if args.output else args.input}/{args.prefix}{path_image.stem}.tiff"
+        io.imsave(path_output, format_function(deconvolved))
+    return None
