8788820a9aba4a8763cb0c015079b8cddca453b5
ShixingWang <wangshixing@wustl.edu>
2025-08-12 16:45:55 -0500

    tried deconvolution with the new ilastik PSFs

---
+++ test/compare_psf.py
@@ -0,0 +1,1 @@
+# plot heatmaps of cross entropies between PSFs
--- test/deconvolve_beads.py
+++ test/deconvolve_beads.py
@@ -11,24 +11,24 @@ channels = ("DAPI","FITC","YFP","TRITC")
 FoVs = (1,2)
 
 # %% devonvolve the cleaned images (uniform backgrounds)
-for c in channels:
+for c in ["FITC","TRITC"]:
     for f in FoVs:
         psf = io.imread(f"data/psf/psf-average_FOV-{f}_{c}.tiff")
         beads = io.imread(f"data/clean/FOV-{f}_{c}.tiff")
 
-        deconvolved = deconvolve(beads,psf,epsilon=1E-3).astype(int)
+        deconvolved = deconvolve(beads,psf,epsilon=1E-8).astype(int)
         if deconvolved.max() > 65535:
             print(f"Warning: FOV-{f}_{c} deconvolved image has values larger than 65535, clipping to 65535.")
             deconvolved = (65535 * deconvolved / deconvolved.max()).astype(int)
 
         io.imsave(
-            f"data/deconvolved/FOV-{f}_{c}_clean_epsilon-1E-3.tiff",
+            f"data/deconvolved/FOV-{f}_{c}_clean_epsilon-1E-8.tiff",
             util.img_as_uint(deconvolved)
         )
 # epsilon ↑: less deconvolved, more like blurry images
 # epsilon ↓: more deconvolved, could give empty images
-# epsilon = 1E-5 is good enough for FITC and TRITC.
-# epsilon = 1E-2 is pretty good for DAPI and YFP.
+# epsilon = 1E-6 is good enough for FITC and TRITC.
+# epsilon = 1E-3 is pretty good for DAPI and YFP.
 
 # %% devonvolve the raw images (non-uniform backgrounds)
 import nd2
@@ -44,7 +44,7 @@ for c,ep in zip(channels,[1E-3,1E-6,1E-3,1E-6]):
             deconvolved = (65535 * deconvolved / deconvolved.max()).astype(int)
         io.imsave(
             f"data/deconvolved/FOV-{f}_{c}_raw_epsilon-{str(ep).replace('.','-')}.tiff",
-            util.img_as_float32(np.real_if_close(deconvolved))
+            util.img_as_uint(np.real_if_close(deconvolved))
         )
 
 # %% looks nice directly on raw images, but we need some quantified metrics.
