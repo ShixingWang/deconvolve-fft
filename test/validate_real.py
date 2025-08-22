3653e24a5934222738e06595f2bef9fece767426
ShixingWang <wangshixing@wustl.edu>
2025-08-22 14:48:44 -0500

    Update validate_real.py

--- test/validate_real.py
+++ test/validate_real.py
@@ -9,12 +9,12 @@ from deconvolve_fft import deconvolve
 
 # %%
 names = [
-    "zStack_PX-VO_FOV-0_camera-blues.nd2",
-    "zStack_PX-VO-ER_FOV-8_camera-bluegreen.nd2",
+    # "zStack_PX-VO_FOV-0_camera-blues",
+    "zStack_PX-VO-ER_FOV-8_camera-bluegreen",
 ]
 # %%
 for name in names:
-    path = Path(f"../organelle-recognize/images/2025-02-23_Mixed1ColorDiploids/raw/{name}")
+    path = Path(f"../organelle-recognize/images/2025-02-23_Mixed1ColorDiploids/raw/{name}.nd2")
     img = nd2.imread(str(path))
     print(name, img.shape)
     for c in range(img.shape[1]):
@@ -35,40 +35,42 @@ for path in Path("data/validate/tiff").glob("c-2*.tiff"):
         )
 # seems like sigma=25 is a good choice
 
-# %% with or without background subtraction
-psf = io.imread("data/psf/psf-mask_FOV-1_FITC.tiff")
-for path in Path("data/validate/tiff").glob("c-3*.tiff"):
+# %% with ~~or without~~ background subtraction
+psf = io.imread("data/psf/psf-max_FOV-1_FITC.tiff")
+# psf = io.imread("data/psf/psf-BornWolf_CFP.tif")
+# for path in Path("data/validate/tiff").glob("c-3*.tiff"):
+for name in names:
+    path = Path(f"data/validate/tiff/c-2_{name}.tiff")
     raw = io.imread(str(path))
 
     bkgd = np.empty_like(raw, dtype=int)
     for z in range(raw.shape[0]):
         bkgd[z] = filters.gaussian(raw[z], sigma=25, preserve_range=True)
     img = raw - bkgd
-    # img[img<0] = 0  # does this make a difference?
+    img[img<0] = 0  # does this make a difference?
+    mean_img = img.mean()
 
-    deconv_raw = deconvolve(raw, psf, epsilon=1E-3)
-    if deconv_raw.max() > 65535:
-        deconv_raw = deconv_raw / deconv_raw.max() * 65535
-    deconv_raw = deconv_raw.astype(np.uint16)
-    io.imsave(
-        f"data/validate/fft_mask/{path.stem}_raw.tiff",
-        util.img_as_uint(deconv_raw)
-    )
-    
-    deconv_img = deconvolve(img, psf, epsilon=1E-3)
-    deconv_img = deconv_img.astype(np.uint16)
-    if deconv_img.max() > 65535:
-        deconv_img = deconv_img / deconv_img.max() * 65535
-    io.imsave(
-        f"data/validate/fft_mask/{path.stem}_clean.tiff",
-        util.img_as_uint(deconv_raw)
-    )
+    for m in range(1,12):
+        deconv_img = deconvolve(img, psf, epsilon=1/10**m)
+        # deconv_img = filters.gaussian(deconv_img, sigma=0.5, preserve_range=True)
+        mean_deconv = deconv_img[deconv_img>0].mean()
+        deconv_img = deconv_img - mean_deconv + mean_img
+        deconv_img[deconv_img < 0] = 0
+        deconv_img[deconv_img > 65535] = 65535
+        deconv_img = deconv_img.astype(np.uint16)
+        io.imsave(
+            f"data/validate/fft/nogaus-max_{path.stem}_epsilon-1E-{m}.tiff",
+            util.img_as_uint(deconv_img)
+        )
+        # epsilon = 1E-3 gives nice results on CFP with FITC PSF
 
 # %% richardson-lucy with theoretical PSF
 for c,camera in zip([1,2,3],["DAPI","CFP","FITC"]):
     psf = io.imread(f"data/psf/psf-BornWolf_{camera}.tif")
     psf = psf / psf.sum()
-    for path in Path("data/validate/tiff").glob(f"c-{c}*.tiff"):
+    # for path in Path("data/validate/tiff").glob(f"c-{c}*.tiff"):
+    for name in names:
+        path = Path(f"data/validate/tiff/c-{c}_{name}.tiff")
         raw = io.imread(str(path))
         bkgd = np.empty_like(raw, dtype=int)
         for z in range(raw.shape[0]):
@@ -79,15 +81,17 @@ for c,camera in zip([1,2,3],["DAPI","CFP","FITC"]):
             deconvolved[deconvolved > 65535] = 65535
             deconvolved = deconvolved.astype(np.uint16)
             io.imsave(
-                f"data/validate/rl/clean_{path.stem}_n-{n}.tiff",
+                f"data/validate/rl/clean-theoretical_{path.stem}_n-{n}.tiff",
                 util.img_as_uint(deconvolved)
             )
         # nearly 4 min for n=30, around 7 min for n=50
 
 # %% richardson-lucy with captured PSF
-for c,camera in zip([1,2,3],["DAPI","CFP","FITC"]):
-    psf = io.imread(f"data/psf/psf-max_FOV-1_{camera}.tif")
-    for path in Path("data/validate/tiff").glob(f"c-{c}*.tiff"):
+for c,camera in zip([3],["FITC"]):
+    psf = io.imread(f"data/psf/psf-max_FOV-1_{camera}.tiff")
+    # for path in Path("data/validate/tiff").glob(f"c-{c}*.tiff"):
+    for name in names:
+        path = Path(f"data/validate/tiff/c-{c}_{name}.tiff")
         raw = io.imread(str(path))
         bkgd = np.empty_like(raw, dtype=int)
         for z in range(raw.shape[0]):
@@ -98,6 +102,7 @@ for c,camera in zip([1,2,3],["DAPI","CFP","FITC"]):
             deconvolved[deconvolved > 65535] = 65535
             deconvolved = deconvolved.astype(np.uint16)
             io.imsave(
-                f"data/validate/rl_theoretical/{path.stem}_n-{n}.tiff",
+                f"data/validate/rl/clean-max_{path.stem}_n-{n}.tiff",
                 util.img_as_uint(deconvolved)
             )
+# %%
